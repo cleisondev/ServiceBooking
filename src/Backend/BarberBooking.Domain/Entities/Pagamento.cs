4d9ae5c3fc7fa49f3428fe33fe22f76f1192daf5
Cleison Nunes <cleison.nunes20@gmail.com>
2025-02-10 22:07:02 -0300

    feature: Enriquecendo entidades

---
+++ src/Backend/BarberBooking.Domain/Entities/Pagamento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBooking.Domain.Entities
+{
+    internal class Pagamento
+    {
+        public int Id { get; set; }
+        public int AgendamentoId { get; set; }
+        public Agendamento Agendamento { get; set; } = null!;
+
+        public decimal Valor { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public StatusPagamento Status { get; set; } = StatusPagamento.Pendente;
+    }
+    public enum StatusPagamento
+    {
+        Pendente,
+        Pago,
+        Cancelado
+    }
+}
---
+++ src/Backend/BarberBooking.Domain/Entities/Servico.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBooking.Domain.Entities
+{
+    internal class Servico
+    {
+        public int Id { get; private set; }
+        public int PrestadorId { get; private set; }
+        public string Nome { get; private set; }
+        public decimal Preco { get; private set; }
+        public TimeSpan Duracao { get; private set; }
+
+        public Servico(int prestadorId, string nome, decimal preco, TimeSpan duracao)
+        {
+            if (string.IsNullOrWhiteSpace(nome)) throw new ArgumentException("Nome do serviço é obrigatório");
+            if (preco <= 0) throw new ArgumentException("Preço deve ser maior que zero");
+            if (duracao.TotalMinutes < 1) throw new ArgumentException("Duração inválida");
+
+            PrestadorId = prestadorId;
+            Nome = nome;
+            Preco = preco;
+            Duracao = duracao;
+        }
+
+        public void AtualizarPreco(decimal novoPreco)
+        {
+            if (novoPreco <= 0) throw new ArgumentException("Preço deve ser maior que zero");
+            Preco = novoPreco;
+        }
+    }
+}
